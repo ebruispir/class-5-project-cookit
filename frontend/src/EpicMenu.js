abe570737f14970724ce3dbcee2d3b2d5a2569c4
Efiloglu <efiloglu0335@gmail.com>
2019-12-01 15:57:19 +0100

    updated firebase

--- frontend/src/EpicMenu.js
+++ frontend/src/EpicMenu.js
@@ -5,8 +5,8 @@ import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import searchIcon from './search-icon.png';
 
 firebase.initializeApp({
-  apiKey: 'AIzaSyDKKpTxO6tDfpZ-twIm9UeKn1jA1u5WyWQ',
-  authDomain: 'cookit-39cc5.firebaseapp.com',
+  apiKey: 'AIzaSyCHCw0nHjUwds38zc9k8rPdkF4UPVzl4DU',
+  authDomain: 'hyf-cookit.firebaseapp.com',
 });
 
 class EpicMenu extends Component {
