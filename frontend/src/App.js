861081b70db649f4dab1c3e8d4516e92fc167df9
Ebru Ispir <ebrukaraispir@gmail.com>
2019-12-03 23:49:12 +0100

    main-page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import EpicMenu from './EpicMenu';
+import Main from "./Main"
 import './App.css'
-import logo from './cookit.png';
+
 
 
 class App extends Component {
   render() {
-    const links = [
-      { label: 'Home', link: '#home', active: true },
-      { label: 'About', link: '#about' },
-      { label: 'Cookit', link: '#cookit' },
-      { label: 'Contact Us', link: '#contact-us' },
 
-    ];
 
     return (
-
-      <div className="container center">
-        <EpicMenu links={links} logo={logo} />
-      </div>
-
+      <>
+        <Main />
+      </>
     );
   }
 }
